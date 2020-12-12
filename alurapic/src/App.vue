42ffd6b386b5334a299b847f33876362918c78b8
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2020-12-11 21:52:46 -0300

    Primeiro Commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="corpo">
+    
+  </div>
+</template>
+
+<script>
+
+export default{
+  }
+
+</script>
+
+<style>
+.corpo{
+  font-family: helvetica;
+  width: 96%;
+  margin: 0 auto;
+}
+</style>
